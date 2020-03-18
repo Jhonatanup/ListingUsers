0246bd61b6007e67e1b0db910674720f84d3fb1b
jhonatanup <jleandroweb@gmail.com>
2020-03-18 18:49:18 -0400

    buscando usuarios e renderizando na tela

--- src/main.js
+++ src/main.js
@@ -1,13 +1,76 @@
-import axios from 'axios'
+import api from './services/api'
 
-class Api{
-    static async getUserInfo(username){
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        }catch(err){
-            console.warn('Erro na API')
-        }
+class App {
+    constructor(){
+        this.users = [];
+        this.formEl = document.getElementById('user-form');
+        this.inputEl= document.querySelector('input[name=user]')
+        this.listEL = document.getElementById('user-list');
+        this.registerHandlers();
     }
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addUsers(event);
+    }
+
+    async addUsers(event){
+        event.preventDefault();
+
+        const userInput = this.inputEl.value;
+        if(userInput.length === 0)
+            return;
+
+        const response = await api.get(`/users/${userInput}`)
+        console.log(response.data)
+        let { name, bio, avatar_url, html_url } = response.data;
+        if(bio === null){
+            bio = 'usuario não possui descrição';
+            console.log(bio);
+        }
+            
+        this.users.push({
+            name,
+            bio,
+            avatar_url,
+            html_url,
+        });
+        console.log(this.users);
+        this.inputEl.value = '';
+        this.render();
+    }
+    render(){
+        this.listEL.innerHTML = '';
+        this.users.forEach( user => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', user.avatar_url);
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(user.name));
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(user.bio))
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', user.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            let itemEl = document.createElement('li');
+            itemEl.appendChild(imgEl);
+            itemEl.appendChild(titleEl);
+            itemEl.appendChild(descriptionEl);
+            itemEl.appendChild(linkEl);
+
+            this.listEL.appendChild(itemEl);
+        })
+    }
+    
 }
-Api.getUserInfo('jhonatanup')
+new App();
+
+// class Api{
+//     static async getUserInfo(username){
+//         try {
+//             const response = await axios.get(`https://api.github.com/users/${username}`);
+//             console.log(response);
+//         }catch(err){
+//             console.warn('Erro na API')
+//         }
+//     }
+// }
+// Api.getUserInfo('jhonatanup')
